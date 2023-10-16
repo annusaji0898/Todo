bd9d4068a942e99c090e10c944e5adf9628df548
ajesh <ajesh.babu@ondace.com>
2023-10-16 17:56:52 +0530

    spelling mistake

--- DisplayTodo.jsx
+++ DisplayTodo.jsx
@@ -11,7 +11,7 @@ function DisplayTodo(props) {
     return (
         <div className="todo-list-item">
             <p onClick= {() => props.toggleComplete(props.todo.id)} className={`${props.todo.completed ? 'completed': " " }`}> {props.todo?.task}</p>
-            <div className="icons">
+            <div className="icon">
             <AiOutlineCheck onClick = {()=>props.editTodo(props.todo.id)} />
             <AiOutlineDelete onClick = {()=>props.deleteTodo(props.todo.id)}/>
             </div>
